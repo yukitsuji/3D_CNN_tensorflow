2fa3ca17167a4f238964f49409ee18f71624b3bd
yukitsuji <yukitsuji020832@gmail.com>
2017-04-05 13:02:49 +0900

    add xml reader

--- input_velodyne.py
+++ input_velodyne.py
@@ -69,25 +69,55 @@ def read_calib_file(calib_path):
                 pass
     return data
 
+def proj_to_velo(calib_data):
+    p0 = calib_data['P0'].reshape(3, 4)
+    velo_to_cam = calib_data["Tr_velo_to_cam"].reshape(3, 4)
+    rect = calib_data["R0_rect"].reshape(3, 3)
+    inv_rect = np.linalg.inv(rect)
+    inv_p0 = np.linalg.inv(p0[:, :3])
+    inv_velo_to_cam = np.linalg.pinv(velo_to_cam)
+    # np.dot(cam_to_velo, np.dot(inv_rect,box[3:]))[:3]
+    # return np.dot(inv_velo_to_cam, np.dot(inv_rect, inv_p0))
+    return np.dot(inv_velo_to_cam, inv_rect)
+
+
 def publish_pc2(velodyne_path, label_path=None, calib_path=None, dataformat="pcd", label_type="txt"):
     p = []
     pc = None
     bounding_boxes = None
     places = None
     rotates = None
+    proj_velo = None
+
     if dataformat == "bin":
         pc = load_pc_from_bin(velodyne_path)
     elif dataformat == "pcd":
         pc = load_pc_from_pcd(velodyne_path)
+
+    if calib_path:
+        calib = read_calib_file(calib_path)
+        proj_velo = proj_to_velo(calib)[:, :3]
+        print proj_velo.shape
+
     obj = []
     if label_path:
         if label_type == "txt": #TODO
             places, size = read_label_from_txt(label_path)
             dummy = np.zeros_like(places)
-            dummy[:, 0] = places[:, 2]
-            dummy[:, 1] = places[:, 0]
-            dummy[:, 2] = places[:, 1]
-            places = dummy
+            print places
+            dummy = places.copy()
+            # dummy[:, 0] = places[:, 2]
+            # dummy[:, 1] = places[:, 0]
+            # dummy[:, 2] = places[:, 1]
+            if calib_path:
+                print dummy.shape
+                print "proj_velo"
+                print proj_velo
+                # places = np.dot(dummy.transpose(), proj_velo)
+                places = np.dot(dummy, proj_velo.transpose())[:, :3]
+                print places.shape
+            else:
+                places = dummy
             rotates = places #TODO
 
         elif label_type == "xml":
@@ -111,44 +141,7 @@ def publish_pc2(velodyne_path, label_path=None, calib_path=None, dataformat="pcd
                     b = (y - w / 2.) + wid / 100.
                     c = (z) + hei / 100.
                     obj.append((a, b, c))
-    # if calib_path:
-    #     calib_data = read_calib_file(calib_path)
-    #     cam_to_velo = calib_data["Tr_imu_to_velo"].reshape(4, 3)
-    #     inv_rect = np.linalg.inv(calib_data["R0_rect"].reshape(3, 3))
-    # for box in bounding_boxes:
-    #     height = box[0]
-    #     width = box[1]
-    #     length = box[2]
-    #     camera_x = box[3]
-    #     camera_y = box[4]
-    #     camera_z = box[5]
-    #     velo_x, velo_y, velo_z = np.dot(cam_to_velo, np.dot(inv_rect,box[3:]))[:3]
-    #     print (camera_x, camera_y, camera_z)
-    #     print (velo_x, velo_y, velo_z)
-    #     obj.append((-camera_z, -camera_x, camera_y))
-    #     obj.append((velo_x / 5., velo_y / 5., velo_z))
 
-    # dimensions   3D object dimensions: height, width, length (in meters)
-    # location     3D object location x,y,z in camera coordinates (in meters)
-    # h = 1.57
-    # w = 1.67
-    # l = 4.14
-    # box = np.array([
-    #     [-l/2, -l/2,  l/2, l/2, -l/2, -l/2,  l/2, l/2], \
-    #     [ w/2, -w/2, -w/2, w/2,  w/2, -w/2, -w/2, w/2], \
-    #     [ 0.0,  0.0,  0.0, 0.0,    h,     h,   h,   h]])
-    #
-    #
-    # for x in range(int(l*100)):
-    #     for y in range(int(w*100)):
-    #         for z in range(int(h*100)):
-    #             a = 1.70 - l / 2. + x / 100.
-    #             b = 4.10 - w / 2. + y / 100.
-    #             c = -2.75 + z / 100. + 1.65
-    #             p.append((a, b, c))
-    #  height width length x, y, z = z, x, y
-    #  1.57 1.67 4.14 -2.75 1.70 4.10
-    # 1.57 1.50 3.68 -1.17 1.65 7.86
     p.append((0, 0, 0))
     print 1
     pub = rospy.Publisher("/points_raw", PointCloud2, queue_size=10000)
@@ -164,18 +157,18 @@ def publish_pc2(velodyne_path, label_path=None, calib_path=None, dataformat="pcd
     header.frame_id = "velodyne"
     points2 = pc2.create_cloud_xyz32(header, obj)
 
-    r = rospy.Rate(1)
+    r = rospy.Rate(0.1)
     while not rospy.is_shutdown():
         pub.publish(points)
         pub2.publish(points2)
         r.sleep()
 
 if __name__ == "__main__":
-    #pcd_path = "/home/katou01/download/training/velodyne/000006.pcd"
-    #label_path = "/home/katou01/download/training/label_2/000006.txt"
-    #calib_path = "/home/katou01/download/training/calib/000006.txt"
-    #publish_pc2(pcd_path, label_path, calib_path=calib_path, dataformat="pcd")
+    pcd_path = "/home/katou01/download/training/velodyne/000010.pcd"
+    label_path = "/home/katou01/download/training/label_2/000010.txt"
+    calib_path = "/home/katou01/download/training/calib/000010.txt"
+    publish_pc2(pcd_path, label_path, calib_path=calib_path, dataformat="pcd")
 
-    bin_path = "/home/katou01/download/2011_09_26/2011_09_26_drive_0001_sync/velodyne_points/data/0000000030.bin"
-    xml_path = "/home/katou01/download/2011_09_26/2011_09_26_drive_0001_sync/tracklet_labels.xml"
-    publish_pc2(bin_path, xml_path, dataformat="bin", label_type="xml")
+    # bin_path = "/home/katou01/download/2011_09_26/2011_09_26_drive_0001_sync/velodyne_points/data/0000000030.bin"
+    # xml_path = "/home/katou01/download/2011_09_26/2011_09_26_drive_0001_sync/tracklet_labels.xml"
+    # publish_pc2(bin_path, xml_path, dataformat="bin", label_type="xml")
